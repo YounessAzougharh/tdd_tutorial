4c571c6c1fb2ce730d8b4e813538ce08de84614f
youness <youness.azougharh@gmail.com>
2017-10-02 18:28:10 +0100

    switch to using unittest for fts,etend to user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,32 @@
+import unittest
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-assert 'Django' in browser.title
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_starting_a_new_to_do_list(self):
+
+#Edith has heard about a new 'to_do' list apps so she goes at home page
+        self.browser.get("http://localhost:8000")
+
+#she noticed the page title mention to_do lists       
+        self.assertIn('to_do', self.browser.title)
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
+
+
+
+    
+
+
+
+#assert 'Django' in browser.title
